cb1574ec6471811bf7fd14d2e8ca18f1ec4972e2
Joris USCLAT <joris.usclat@sfr.fr>
2022-07-13 12:14:09 +1000

    add _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '../scss/global.scss';
 
+/* 
+Persisting layout between page changes
+Keeping state when navigating pages
+Custom error handling using componentDidCatch
+Inject additional data into pages
+Add global CSS
+*/
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
