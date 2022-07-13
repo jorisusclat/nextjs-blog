4bd373007ad7f873a0b50b98d8106ed1583b86f1
Joris USCLAT <joris.usclat@sfr.fr>
2022-07-14 00:25:41 +1000

    add fr lang

--- next.config.js
+++ next.config.js
@@ -5,16 +5,12 @@ module.exports = {
     includePaths: [path.join(__dirname, 'scss')],
   },
   i18n: {
-    locales: ['en-US', /* 'fr' */],
+    locales: ['en-US', 'fr'],
     defaultLocale: 'en-US',
     domains: [
       {
         domain: 'nextjs-blog-liart-iota.vercel.app',
         defaultLocale: 'en-US',
-      },
-      {
-        domain: 'nextjs-blog-liart-iota.vercel.app.fr',
-        defaultLocale: 'fr',
       },
     ],
   },
